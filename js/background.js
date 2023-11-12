c1516aedaac5cfb3548d660feeba928e916c518c
eastsky0614 <92dongho@gmail.com>
2023-11-13 01:14:21 +0900

    Add CSS

--- js/background.js
+++ js/background.js
@@ -2,6 +2,17 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
+// Create a div element to serve as the background container
+const backgroundContainer = document.createElement("div");
+backgroundContainer.classList.add("background-container");
+
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
+
+// Append the background image to the container
+backgroundContainer.appendChild(bgImage);
+
+// Append the background container to the body
+document.body.appendChild(backgroundContainer);
+
+// document.body.appendChild(bgImage);
